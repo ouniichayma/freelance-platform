57a1027f5428ec70c6c543fcec0390e43bdeb03c
ouni chayma <ouniichayma@gmail.com>
2025-05-14 11:51:39 +0200

    Initialisation du projet Spring Boot avec les dépendances

---
+++ freelance-platform/src/main/java/org/example/freelanceplatform/FreelancePlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.freelanceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelancePlatformApplication.class, args);
+    }
+
+}
---
+++ freelance-platform/src/test/java/org/example/freelanceplatform/FreelancePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.freelanceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
